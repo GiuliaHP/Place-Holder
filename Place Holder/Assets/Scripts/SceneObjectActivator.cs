e0bd180ebc7e1748e2f11a006abfe860771316ed
Giu <giulia.perucca@gmail.com>
2025-01-06 21:15:03 +0100

    Ajout terrain neige

--- Place Holder/Assets/Scripts/SceneObjectActivator.cs
+++ Place Holder/Assets/Scripts/SceneObjectActivator.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneObjectActivator : MonoBehaviour
 {
     public GameObject objectToActivate;
+    public CircleTransition transitionScript;
+    public string newScene = "SnowScene";
 
     private void Start()
     {
         // Vérifie si le booléen global est activé
         if (PersistentManager.Instance != null && PersistentManager.Instance.triggerObjectInOtherScene)
         {
-            // Active l'objet et réinitialise le booléen si nécessaire
+            transitionScript.targetScene = newScene;
             objectToActivate.SetActive(true);
             PersistentManager.Instance.triggerObjectInOtherScene = false;
         }
